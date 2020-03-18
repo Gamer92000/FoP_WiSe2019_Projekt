0f7cf18baf27ce6a06de21e1cfae0c4540b0de44
Gamer92000 <julianimhof00@gmail.com>
2020-03-18 18:03:04 +0100

    fancyfy code | use table font
    I still do NOT like the font!

--- Carcassonne_Vorlage/src/fop/view/gui/CellRendererHighscore.java
+++ Carcassonne_Vorlage/src/fop/view/gui/CellRendererHighscore.java
@@ -6,35 +6,20 @@ import java.awt.Component;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 
+@SuppressWarnings("serial")
 public class CellRendererHighscore extends DefaultTableCellRenderer{
-	
 	
 	public CellRendererHighscore() {
 		setOpaque(true);
-		}
+	}
 		  
-		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-		  
-			if (row == 0) {
-				setBackground(new Color(255,215,0));
-			} else 
-			if (row == 1) {
-				setBackground(new Color(192,192,192));
-			} else
-			if (row == 2) {
-				setBackground(new Color(210,105,30));
-			} else {
-				
-				if(Math.floorMod(row, 2) == 0)
-					setBackground(new Color(245, 245, 245));
-				if(Math.floorMod(row, 2) == 1)
-					setBackground(Color.white);
-			}
-			
-			setText((String)value);
-			
-			return this;
-		
-		} 
-
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+		if (row == 0) setBackground(new Color(255,215,0));
+		else if (row == 1) setBackground(new Color(192,192,192));
+		else if (row == 2) setBackground(new Color(210,105,30));
+		else setBackground(Math.floorMod(row, 2) == 0 ? new Color(245, 245, 245) : Color.white);
+		setText((String)value);
+		setFont(table.getFont());
+		return this;
+	} 
 }
